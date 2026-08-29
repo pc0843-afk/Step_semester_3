f7c86af9224717dbf477ff2791fb7a0bc21771ea
pc0843-afk <pc0843@srmist.edu.in>
2026-08-29 11:21:26 +0530

    Implement NumberClassifier to classify integers

---
+++ control_flow/assigment_problems/NumberClassifier.java
@@ -0,0 +1,16 @@
+public class NumberClassifier {
+
+    public static void classifyNumber(int number) {
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+
+    public static void main(String[] args) {
+        classifyNumber(-4);
+    }
+}
